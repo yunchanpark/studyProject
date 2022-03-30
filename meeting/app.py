691920c49207e92416d6c2e01d3642590b1168f5
yunchanpark <96051437+yunchanpark@users.noreply.github.com>
2022-03-30 16:23:22 +0900

    yunchan#5

--- meeting/app.py
+++ meeting/app.py
@@ -14,39 +14,49 @@ SECRET_KEY = 'HelloFlask'
 
 app = Flask(__name__)
 # 메인페이지
+
+
 @app.route('/home')
 def home():
     return render_template('index.html')
 
 # 로그인
+
+
 @app.route('/login')
 def login():
     return render_template('member/login.html')
+
 
 @app.route('/api/login', methods=['POST'])
 def api_login():
     id_receive = request.form['id']
     password_receive = request.form['password']
-    
-    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-    result = db.user.find_one({'user_id': id_receive, 'password': password_hash})
-    
+
+    password_hash = hashlib.sha256(
+        password_receive.encode('utf-8')).hexdigest()
+    result = db.user.find_one(
+        {'user_id': id_receive, 'password': password_hash})
+
     if result is not None:
         payload = {
             'id': id_receive,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes = 60 * 60 * 24)
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60 * 60 * 24)
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-        
+
         return jsonify({'success': True, 'token': token})
     else:
         return jsonify({'success': False, 'msg': '아이디/비밀번호가 일치하지 않습니다'})
-    
+
 # 아이디 중복 체크
+
+
 def isDuplicate(_id):
     if db.user.find_one({'user_id': _id}):
         return True
     return False
+
 
 @app.route('/api/id_check', methods=['POST'])
 def idCheck():
@@ -56,9 +66,12 @@ def idCheck():
     return jsonify({'success': True, 'msg': '사용 가능한 아이디입니다.'})
 
 # 회원가입
+
+
 @app.route('/register', methods=['GET'])
 def register():
     return render_template('member/register.html')
+
 
 @app.route('/api/register', methods=['POST'])
 def api_register():
@@ -70,21 +83,24 @@ def api_register():
     _password_hash = hashlib.sha256(_password.encode('utf-8')).hexdigest()
     _name = result['nick_name']
     _gender = result['gender']
-    
-    db.user.insert_one({'user_id': _id, 'password': _password_hash, 'name': _name, 'gender': _gender})
+
+    db.user.insert_one(
+        {'user_id': _id, 'password': _password_hash, 'name': _name, 'gender': _gender})
     return jsonify({'success': True, 'msg': '로그인 페이지로 이동합니다.'})
+
 
 @app.route('/create')
 def create():
     return render_template('broad/create.html')
+
 
 @app.route('/api/create', methods=['POST'])
 def create_article():
     # 1. 클라이언트로부터 데이터를 받기
     uId_receive = request.form['uId_give']
     uid = jwt.decode(uId_receive, SECRET_KEY, algorithms='HS256')
-    title_receive = request.form['title_give'] 
-    intro_receive = request.form['intro_give'] 
+    title_receive = request.form['title_give']
+    intro_receive = request.form['intro_give']
     people_receive = request.form['people_give']
     sdate_receive = request.form['sdate_give']
     edate_receive = request.form['edate_give']
@@ -92,12 +108,14 @@ def create_article():
     splace_receive = request.form['splace_give']
     desc_receive = request.form['desc_give']
 
-    article = {'uId': uid['id'], 'title': title_receive, 'intro': intro_receive, 'people': people_receive, 'sdate': sdate_receive, 'edate': edate_receive, 'fpalce': fplace_receive, 'splace': splace_receive, 'desc': desc_receive}
-    
+    article = {'uId': uid['id'], 'title': title_receive, 'intro': intro_receive, 'people': people_receive,
+               'sdate': sdate_receive, 'edate': edate_receive, 'fpalce': fplace_receive, 'splace': splace_receive, 'desc': desc_receive}
+
     # 3. mongoDB에 데이터를 넣기
     dbpost.articles.insert_one(article)
 
     return jsonify({'result': 'success'})
+
 
 @app.route('/api/create', methods=['GET'])
 def read_articles():
@@ -105,10 +123,11 @@ def read_articles():
     result = []
     pre_result = list(dbpost.articles.find({}))
     for document in pre_result:
-        document['_id'] = str(document['_id']) # string 변환
+        document['_id'] = str(document['_id'])  # string 변환
         result.append(document)
     # 2. articles라는 키 값으로 article 정보 보내주기
     return jsonify({'result': 'success', 'articles': result})
+
 
 @app.route('/api/edit', methods=['POST'])
 def post_edited_article():
@@ -122,16 +141,27 @@ def post_edited_article():
     new_fplace_receive = request.form['new_fplace_give']
     new_splace_receive = request.form['new_splace_give']
     new_desc_receive = request.form['new_desc_give']
-    dbpost.articles.update_one({'_id': ObjectId(postId_receive)}, {'$set': {'title': newTitle_receive, 'content': newContent_receive}})
+    dbpost.articles.update_one({'_id': ObjectId(postId_receive)}, {
+                               '$set': {'title': newTitle_receive, 'content': newContent_receive}})
 
     return jsonify({'result': 'success'})
+
 
 @app.route('/api/delete', methods=['POST'])
 def delete_article():
-    postId_receive = request.form['postId_give']
-    dbpost.articles.delete_one({'_id': ObjectId(postId_receive)})
-    # 3. 성공하면 success 메시지를 반환합니다.
-    return jsonify({'result': 'success'})
+    try:
+        postId_receive = request.form['postId_give']
+        token = request.form['token']
+        uid = jwt.decode(token, SECRET_KEY, algorithms='HS256')
+        dbpost.articles.delete_many({'_id': ObjectId(postId_receive), 'uId': uid['id']})
+        # 3. 성공하면 success 메시지를 반환합니다.
+        return jsonify({'result': 'success'})
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+        # 만약 해당 token이 올바르게 디코딩되지 않는다면, 아래와 같은 코드를 실행합니다.
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
