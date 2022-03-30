67505c2dec87890b0ee556e652df4a927c35cd28
yunchanpark <96051437+yunchanpark@users.noreply.github.com>
2022-03-30 19:21:58 +0900

    yunchan#7
    모임 수정, 모임 상세페이지

--- meeting/app.py
+++ meeting/app.py
@@ -1,4 +1,3 @@
-from re import A
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 import jwt
 import hashlib
@@ -7,27 +6,23 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 client = MongoClient('localhost', 27017)
 db = client.dbMember
-dbpost = client.dbpost
+dbPost = client.dbPost
 
 
 SECRET_KEY = 'HelloFlask'
 
 app = Flask(__name__)
+
 # 메인페이지
-
-
 @app.route('/home')
 def home():
-    articles = dbpost.articles.find()
+    articles = dbPost.articles.find()
     return render_template('index.html', articles=articles)
 
 # 로그인
-
-
 @app.route('/login')
 def login():
     return render_template('member/login.html')
-
 
 @app.route('/api/login', methods=['POST'])
 def api_login():
@@ -51,13 +46,10 @@ def api_login():
         return jsonify({'success': False, 'msg': '아이디/비밀번호가 일치하지 않습니다'})
 
 # 아이디 중복 체크
-
-
 def isDuplicate(_id):
     if db.user.find_one({'user_id': _id}):
         return True
     return False
-
 
 @app.route('/api/id_check', methods=['POST'])
 def idCheck():
@@ -67,12 +59,9 @@ def idCheck():
     return jsonify({'success': True, 'msg': '사용 가능한 아이디입니다.'})
 
 # 회원가입
-
-
 @app.route('/register', methods=['GET'])
 def register():
     return render_template('member/register.html')
-
 
 @app.route('/api/register', methods=['POST'])
 def api_register():
@@ -89,11 +78,17 @@ def api_register():
         {'user_id': _id, 'password': _password_hash, 'name': _name, 'gender': _gender})
     return jsonify({'success': True, 'msg': '로그인 페이지로 이동합니다.'})
 
+# 모임 상세페이지
+@app.route('/api/detail')
+def detail():
+    postId_receive = request.args.get('postId_give')
+    article = dbPost.articles.find_one({'_id': ObjectId(postId_receive)})
+    return render_template('broad/detail.html', article = article)
 
+# 모임 생성
 @app.route('/create')
 def create():
     return render_template('broad/create.html')
-
 
 @app.route('/api/create', methods=['POST'])
 def create_article():
@@ -110,40 +105,53 @@ def create_article():
     desc_receive = request.form['desc_give']
 
     article = {'uId': uid['id'], 'title': title_receive, 'intro': intro_receive, 'people': people_receive,
-               'sdate': sdate_receive, 'edate': edate_receive, 'fpalce': fplace_receive, 'splace': splace_receive, 'desc': desc_receive}
+                'sdate': sdate_receive, 'edate': edate_receive, 'fplace': fplace_receive,
+                'splace': splace_receive, 'desc': desc_receive}
 
     # 3. mongoDB에 데이터를 넣기
-    dbpost.articles.insert_one(article)
+    dbPost.articles.insert_one(article)
 
     return jsonify({'result': 'success'})
 
+# 모임 수정
+@app.route('/api/update', methods=['POST'])
+def update_article():
+    # 1. 클라이언트로부터 데이터를 받기
+    postId_receive = request.form['postId_give']
+    uId_receive = request.form['uId_give']
+    uid = jwt.decode(uId_receive, SECRET_KEY, algorithms='HS256')
+    title_receive = request.form['title_give']
+    intro_receive = request.form['intro_give']
+    people_receive = request.form['people_give']
+    sdate_receive = request.form['sdate_give']
+    edate_receive = request.form['edate_give']
+    fplace_receive = request.form['fplace_give']
+    splace_receive = request.form['splace_give']
+    desc_receive = request.form['desc_give']
 
-@app.route('/api/create', methods=['GET'])
-def read_articles():
-    # 1. mongoDB에서 모든 데이터 조회해오기 (Read)
-    result = []
-    pre_result = list(dbpost.articles.find({}))
-    for document in pre_result:
-        document['_id'] = str(document['_id'])  # string 변환
-        result.append(document)
-    # 2. articles라는 키 값으로 article 정보 보내주기
-    return jsonify({'result': 'success', 'articles': result})
+    article = {'title': title_receive, 'intro': intro_receive, 'people': people_receive,
+                'sdate': sdate_receive, 'edate': edate_receive, 'fplace': fplace_receive, 
+                'splace': splace_receive, 'desc': desc_receive}
+    print(article)
+    # 3. mongoDB에 데이터를 넣기
+    dbPost.articles.update_one({'_id': ObjectId(postId_receive), 'uId': uid['id']}, {"$set": article})
+
+    return jsonify({'result': 'success'})
 
 @app.route('/api/edit', methods=['GET'])
 def post_edited_article():
     postId_receive = request.args.get('postId_give')
-    article = dbpost.articles.find_one({'_id': ObjectId(postId_receive)})
+    article = dbPost.articles.find_one({'_id': ObjectId(postId_receive)})
     return render_template('broad/edit.html', article = article)
 
-
-
+# 모임 삭제
 @app.route('/api/delete', methods=['POST'])
 def delete_article():
     try:
         postId_receive = request.form['postId_give']
         token = request.form['token']
         uid = jwt.decode(token, SECRET_KEY, algorithms='HS256')
-        dbpost.articles.delete_many(
+        dbPost.articles.delete_many(
             {'_id': ObjectId(postId_receive), 'uId': uid['id']})
         # 3. 성공하면 success 메시지를 반환합니다.
         return jsonify({'result': 'success'})
@@ -152,7 +160,6 @@ def delete_article():
         # 만약 해당 token이 올바르게 디코딩되지 않는다면, 아래와 같은 코드를 실행합니다.
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
-
 
 if __name__ == '__main__':
     app.run(debug=True)
